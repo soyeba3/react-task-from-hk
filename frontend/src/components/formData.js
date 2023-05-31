6d940589c3dad45e4b646b1e0213e19d4eb5e065
soyeba3 <soyebchandani@gmail.com>
2023-06-01 01:16:42 +0600

    complete homepage design

---
+++ frontend/src/components/formData.js
@@ -0,0 +1,47 @@
+export const selectors = [
+  {
+    id: 1,
+    value: "Manufacturing",
+    name: "Manufacturing",
+  },
+  {
+    id: 2,
+    value: "Others",
+    name: "Others",
+  },
+  {
+    id: 3,
+    value: "Service",
+    name: "Service",
+  },
+];
+
+export const subSelectors = [
+  {
+    id: 1,
+    sectorId: 1,
+    value: "Construction materials",
+    name: "Construction materials",
+  },
+  {
+    id: 2,
+    sectorId: 1,
+    value: "Electronics and Optics",
+    name: "Electronics and Optics",
+  },
+  {
+    id: 3,
+    sectorId: 1,
+    value: "Food and Beverage",
+    name: "Food and Beverage",
+  },
+];
+
+export const subSelectors1 = [
+  {
+    id: 1,
+    subSelectorsId: 3,
+    value: "Bakery & confectionery products",
+    name: "Bakery & confectionery products",
+  },
+];
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,93 @@
+import { useEffect, useState } from "react";
+import { selectors, subSelectors, subSelectors1 } from "../components/formData";
+
+const Home = () => {
+  const [sectorName, setSectorName] = useState("");
+  const [subSectorName, setSubSectorName] = useState("");
+  const [subSector, setSubSector] = useState([]);
+  const [subSector1, setSubSector1] = useState([]);
+
+  const handleSectorName = (e) => {
+    setSectorName(e.target.value);
+    setSubSectorName("");
+  };
+
+  useEffect(() => {
+    const filteredSubsectors = subSelectors?.filter(
+      (element) => element?.sectorId == sectorName?.split(",")[1]
+    );
+    setSubSector(filteredSubsectors ? filteredSubsectors : []);
+  }, [sectorName]);
+
+  useEffect(() => {
+    const filteredSubsectors1 = subSelectors1?.filter(
+      (element) => element?.subSelectorsId == subSectorName?.split(",")[1]
+    );
+    setSubSector1(filteredSubsectors1);
+  }, [subSectorName, sectorName]);
+
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <form className="w-5/6 h-full sm:w-1/2 bg-cyan-100 p-[6%] flex flex-col gap-2 rounded-md">
+        <label className="text-xs">
+          Please enter your name and pick the Sectors you are currently involved
+          in.
+        </label>
+        <label>Name :</label>
+        <input
+          type="text"
+          required
+          placeholder="Enter your name"
+          className="border-solid border-2 border-cyan-200 w-full inline-block px-2 py-1 outline-none"
+        />
+        <label>Sectors :</label>
+        <select
+          required
+          onChange={handleSectorName}
+          className="text-sm py-1 border-solid border-2 border-cyan-200 outline-none"
+        >
+          {" "}
+          <option disabled>Select</option>
+          {selectors?.map((option) => (
+            <option key={option.id} value={[option.value, option.id]}>
+              {option.name}
+            </option>
+          ))}
+        </select>
+        {subSector.length > 0 && (
+          <select
+            required
+            onChange={(e) => setSubSectorName(e.target.value)}
+            className="text-sm py-1 border-solid border-2 border-cyan-200 outline-none"
+          >
+            {subSector?.map((option) => (
+              <option key={option.id} value={[option.value, option.id]}>
+                {option.name}
+              </option>
+            ))}
+          </select>
+        )}
+        {subSector1.length > 0 && (
+          <select
+            required
+            className="text-sm py-1 border-solid border-2 border-cyan-200 outline-none"
+          >
+            {subSector1?.map((option) => (
+              <option key={option.id} value={[option.value, option.id]}>
+                {option.name}
+              </option>
+            ))}
+          </select>
+        )}
+        <label>
+          <input required type="checkbox" name="" id="" /> Agree to terms
+        </label>
+        <button type="submit" className="w-full bg-cyan-600 text-white py-1">
+          Save
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Home;
